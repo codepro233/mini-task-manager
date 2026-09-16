0e67761759cc1cbf67d33a85ffcb81c9fb7ee482
Tomide <abereoluwatomide54@gmail.com>
2026-09-16 14:14:43 +0100

    Dockerize mini task manager

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB
+});
+
+app.get("/api/tasks", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM tasks ORDER BY id"
+    );
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to get tasks"
+    });
+  }
+});
+
+app.post("/api/tasks", async (req, res) => {
+  try {
+    const { title } = req.body;
+
+    const result = await pool.query(
+      "INSERT INTO tasks (title, completed) VALUES ($1, $2) RETURNING *",
+      [title, false]
+    );
+
+    res.json({
+      message: "Task created",
+      task: result.rows[0]
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Failed to create task"
+    });
+  }
+});
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Server running on port 5000");
+});
---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  async function fetchTasks() {
+    const response = await fetch("http://localhost:5000/api/tasks");
+
+    const data = await response.json();
+
+    setTasks(data);
+  }
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost:5000/api/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: task
+      })
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    setTasks([...tasks, data.task]);
+
+    setTask("");
+  }
+
+
+  
+
+  return (
+    <div>
+      <h1>Mini Task Manager</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={task}
+          onChange={(event) => setTask(event.target.value)}
+        />
+
+        <button type="submit">Add Task</button>
+      </form>
+
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task.title}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
